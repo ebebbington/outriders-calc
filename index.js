3a2405c7cca340b963c7da322b11698d1896d595
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-04-25 21:13:32 +0100

    add favicon; update styling

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ const powerBrackets = {
   30: 1.10365, // multiplier for levels from 31 to 40, incl 31 and 40,
   40: 1.1552, // multiplier for levels from 41 to 50, incl 41 and 50
 };
+const armorBrackets = {
+    10: 1.102, // multiplier for levels from 31 to 40, incl 31 and 40,
+    20: 1.102, // multiplier for levels from 31 to 40, incl 31 and 40,
+    30: 1.10 // multiplier for levels from 31 to 40, incl 31 and 40,
+}
 window.addEventListener("DOMContentLoaded", () => {
   // On any of the item fields changing, try update the items total power
   const itemInputs = {
     1: {
       $level: document.getElementById("item-1-level"),
       $power: document.getElementById("item-1-power"),
+      $result: document.getElementById("item-1-result"),
     },
     2: {
       $level: document.getElementById("item-2-level"),
       $power: document.getElementById("item-2-power"),
+      $result: document.getElementById("item-2-result"),
     },
   };
   itemInputs[1].$level.addEventListener("keyup", () => {
@@ -36,9 +43,8 @@ window.addEventListener("DOMContentLoaded", () => {
     //
     // VALIDATION
     //
-
     // If theres no bracket, eg user has only typed "1" for "19" for the level, dont change anything
-    if (itemLevel.length === 1) {
+    if (itemLevel.toString().length === 1) {
       return false;
     }
     // If either level or power fields are empty, do nothing
@@ -47,7 +53,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
     // If item level is over the cap, return false
     if (itemLevel > maxItemLevel) {
-      return false; // TODO SHOW ERROR TO USER THEY CANT GO OVER MAX
+      return false;
     }
 
     let updatedPower = itemPower;
@@ -66,5 +72,27 @@ window.addEventListener("DOMContentLoaded", () => {
     displayWhichItemIsStronger();
   }
   function displayWhichItemIsStronger() {
+    const itemOneResult = Number(itemInputs[1].$result.value);
+    const itemTwoResult = Number(itemInputs[2].$result.value);
+    // If either fields dont have a value of some sort, then clear the highlight
+    if (
+      !itemOneResult || !itemTwoResult || isNaN(itemOneResult) ||
+      isNaN(itemTwoResult)
+    ) {
+      itemInputs[1].$result.classList.remove("highlight-good", "highlight-bad");
+      itemInputs[1].$result.classList.remove("highlight-good", "highlight-bad");
+      return false;
+    }
+    if (itemOneResult > itemTwoResult) {
+      itemInputs[1].$result.classList.add("highlight-good");
+      itemInputs[2].$result.classList.add("highlight-bad");
+      itemInputs[1].$result.classList.remove("highlight-bad");
+      itemInputs[2].$result.classList.remove("highlight-good");
+    } else {
+      itemInputs[1].$result.classList.add("highlight-bad");
+      itemInputs[2].$result.classList.add("highlight-good");
+      itemInputs[1].$result.classList.remove("highlight-good");
+      itemInputs[2].$result.classList.remove("highlight-bad");
+    }
   }
 });
