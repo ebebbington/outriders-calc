acdec22c04abaf686c1853e6f6a43c8e07cde78c
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-04-26 10:24:42 +0100

    fix make website a server script

--- make_website_a_server.ts
+++ make_website_a_server.ts
@@ -8,6 +8,19 @@ class HomeResource extends Drash.Http.Resource {
   }
 }
 
+class A extends Drash.Http.Resource {
+  static paths = ["/index.css", "/index.js"]
+  public GET () {
+    const path = this.request.url_path
+    const raw = Deno.readFileSync("." + path)
+    const decoded = new TextDecoder().decode(raw)
+    this.response.body = decoded
+    const contentType = path.includes("css") ? "text/css" : "application/javascript"
+    this.response.headers.set("Content-Type", contentType)
+    return this.response
+  }
+}
+
 const tengine = Tengine({
   render: (...args: unknown[]): boolean => {
     return false;
@@ -17,7 +30,7 @@ const tengine = Tengine({
 
 const server = new Drash.Http.Server({
   directory: ".",
-  resources: [HomeResource],
+  resources: [HomeResource, A],
   middleware: {
     after_resource: [tengine]
   },
