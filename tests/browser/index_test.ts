42028e775e4bafbfb2cd395ee604ee36183a2329
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-04-26 11:55:26 +0100

    fix ci

--- tests/browser/index_test.ts
+++ tests/browser/index_test.ts
@@ -1,48 +1,50 @@
 import { HeadlessBrowser, Rhum } from "../deps.ts";
 
 Rhum.testPlan("Home page", () => {
-  // Rhum.testSuite("Item Type Switch", async () => {
-  //     Rhum.testCase("Weapon type is selected by default", async () => {
-  //     const Sinco = new HeadlessBrowser()
-  //     await Sinco.build()
-  //     await Sinco.goTo("http://localhost:1337")
-  //     const val = await Sinco.getInputValue("input#weapon")
-  //     await Sinco.done()
-  //     Rhum.asserts.assertEquals(val, "yes")
-  //     })
-  //     Rhum.testCase("Armor type is not selected by default", async () => {
-  //         const Sinco = new HeadlessBrowser()
-  //     await Sinco.build()
-  //     await Sinco.goTo("http://localhost:1337")
-  //     const val = await Sinco.getInputValue("input#armour")
-  //     await Sinco.done()
-  //     Rhum.asserts.assertEquals(val, "no")
-  //     })
-  //     Rhum.testCase("Can switch type back and forth", async () => {
-  //         const Sinco = new HeadlessBrowser()
-  //     await Sinco.build()
-  //     await Sinco.goTo("http://localhost:1337")
-  //     await Sinco.click("input#armour")
-  //     async function getSwitchCheckedProps (): Promise<[boolean, boolean]> {
-  //         const result = await Sinco.evaluatePage(() => {
-  //         // @ts-ignore
-  //         const elem = document.querySelector("input#armour")
-  //         // @ts-ignore
-  //         const elem2 = document.querySelector("input#weapon")
-  //         // @ts-ignore
-  //         return [elem2.checked, elem.checked]
-  //         }) as [boolean, boolean]
-  //         return result
-  //     }
-  //     const firstSwitchChecked = await getSwitchCheckedProps()
-  //     await Sinco.click("input#weapon")
-  //     const secondSwitchChecked = await getSwitchCheckedProps()
-  //     await Sinco.done()
-  //     Rhum.asserts.assertEquals(firstSwitchChecked, [false, true])
-  //     Rhum.asserts.assertEquals(secondSwitchChecked, [true, false])
-  //     })
-
-  // })
+  Rhum.testSuite("Item Type Switch", () => {
+    Rhum.testCase("Weapon type is selected by default", async () => {
+      const Sinco = new HeadlessBrowser();
+      await Sinco.build();
+      await Sinco.goTo("http://localhost:1337");
+      const val = await Sinco.getInputValue("input#weapon");
+      await Sinco.done();
+      Rhum.asserts.assertEquals(val, "yes");
+    });
+    Rhum.testCase("Armor type is not selected by default", async () => {
+      const Sinco = new HeadlessBrowser();
+      await Sinco.build();
+      await Sinco.goTo("http://localhost:1337");
+      const val = await Sinco.getInputValue("input#armour");
+      await Sinco.done();
+      Rhum.asserts.assertEquals(val, "no");
+    });
+    Rhum.testCase("Can switch type back and forth", async () => {
+      const Sinco = new HeadlessBrowser();
+      await Sinco.build();
+      await Sinco.goTo("http://localhost:1337");
+      await Sinco.click("input#armour");
+      async function getSwitchCheckedProps(): Promise<[boolean, boolean]> {
+        const result = await Sinco.evaluatePage(() => {
+          // deno-lint-ignore ban-ts-comment Deno broke usage of the tsconfig we need again...
+          // @ts-ignore
+          const elem = document.querySelector("input#armour");
+          // deno-lint-ignore ban-ts-comment Deno broke usage of the tsconfig we need again...
+          // @ts-ignore
+          const elem2 = document.querySelector("input#weapon");
+          // deno-lint-ignore ban-ts-comment Deno broke usage of the tsconfig we need again...
+          // @ts-ignore
+          return [elem2.checked, elem.checked];
+        }) as [boolean, boolean];
+        return result;
+      }
+      const firstSwitchChecked = await getSwitchCheckedProps();
+      await Sinco.click("input#weapon");
+      const secondSwitchChecked = await getSwitchCheckedProps();
+      await Sinco.done();
+      Rhum.asserts.assertEquals(firstSwitchChecked, [false, true]);
+      Rhum.asserts.assertEquals(secondSwitchChecked, [true, false]);
+    });
+  });
   Rhum.testSuite("Item compare", () => {
     Rhum.testCase(
       "Filling in level and power for item 1 shows result",
