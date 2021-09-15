a99fc2a1e50810159f5d351a6bee06efd98c6ab3
ebebbington <ebebbington@users.noreply.github.com>
2021-09-15 00:07:33 +0000

    choore: Update dependencies

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,4 +1,4 @@
-export { Rhum } from "https://deno.land/x/rhum@v1.1.9/mod.ts";
-export { HeadlessBrowser } from "https://deno.land/x/sinco@v1.1.1/mod.ts";
-export { Drash } from "https://deno.land/x/drash@v1.4.4/mod.ts";
-export { Tengine } from "https://deno.land/x/drash_middleware@v0.7.6/tengine/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.11/mod.ts";
+export { HeadlessBrowser } from "https://deno.land/x/sinco@v2.0.0/mod.ts";
+export { Drash } from "https://deno.land/x/drash@v1.5.1/mod.ts";
+export { Tengine } from "https://deno.land/x/drash_middleware@v0.7.9/tengine/mod.ts";
